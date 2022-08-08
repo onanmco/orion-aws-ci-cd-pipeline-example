124335d89089e80d82133752ebbc2962141f70c0
onanmco <mustafa.onan@orioninc.com>
2022-08-08 13:18:55 +0300

    new update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/hello", (req, res) => {
-    res.send("Hello world!");
+    res.send("Hello from Orion Innovation!");
 });
 
 app.listen(3000);
